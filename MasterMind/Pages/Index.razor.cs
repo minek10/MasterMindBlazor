25a02e631dae1c45bc6890be28b464870e505c66
Riccardo Carroyer <71042071+minek10@users.noreply.github.com>
2023-10-08 20:47:37 +0200

    Index change

---
+++ MasterMind/Pages/Index.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MasterMind.Pages
+{
+    public partial class Index
+    {
+       [Inject] public NavigationManager NavigationManager { get; set; }
+
+        public void RedirectToGame()
+        {
+            NavigationManager.NavigateTo("/game");
+        }
+    }
+}
